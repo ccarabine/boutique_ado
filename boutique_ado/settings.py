8970888da2cb5e36f2a2d89fe4f071c46f479a49
ccarabine <ccarabine12@gmail.com>
2022-03-07 08:19:34 +0000

    Add email settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -214,3 +214,15 @@ STRIPE_WH_SECRET =  os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER_CK = os.environ.get('EMAIL_HOST_USER_CK')
+    EMAIL_HOST_PASSWORD_CK = os.environ.get('EMAIL_HOST_PASS_CK')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER_CK')
