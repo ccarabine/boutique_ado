966efe02accb77e26c11270f0c55cfb744700212
ccarabine <ccarabine12@gmail.com>
2022-03-07 08:44:15 +0000

    Update email password

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -216,13 +216,14 @@ DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    DEFAULT_FROM_EMAIL = "Corona Virus Forum team <noreply@coronaforum.com>"
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_USE_TLS = True
+    EMAIL_HOST = "smtp.gmail.com"
     EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER_CK = os.environ.get('EMAIL_HOST_USER_CK')
-    EMAIL_HOST_PASSWORD_CK = os.environ.get('EMAIL_HOST_PASS_CK')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER_CK')
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER_CK")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD_CK")
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER_CK")
+    
